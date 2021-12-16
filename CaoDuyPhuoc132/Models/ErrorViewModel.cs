8653c966ec7e3c02ec09ee790125e9ff42934f7e
unknown <caophc925@gmail.com>
2021-12-16 07:33:00 +0700

    First_commit_CDP132

---
+++ CaoDuyPhuoc132/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CaoDuyPhuoc132.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
